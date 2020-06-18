2c3cc276ebdd64ac409e80bce3cb9a8a27d62aaf
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-18 23:37:02 +0300

    error

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,21 +3,22 @@ import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
-// const query = useStaticQuery(graphql`
-//   {
-//     allStrapiJobs(sort: {fields: stripiId, order: DESC}) {
-//       nodes {
-//         strpiId
-//         company
-//         date
-//         position
-//         desc {
-//           id
-//         }
-//       }
-//     }
-//   }
-// `)
+const query = graphql`
+  {
+    allStrapiJobs(sort: {fields: stripiId, order: DESC}) {
+      nodes {
+        strpiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 const Jobs = () => {
   return <h2>jobs component</h2>
 }
